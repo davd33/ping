98562ab3a9db2f1d1ef74f6b78495487fbb2cef1
David Rueda <davd33@gmail.com>
2024-03-19 23:16:01 +0100

    frog game, 2D sprites, collisions

--- src/ping.scm
+++ src/ping.scm
@@ -8,14 +8,12 @@
 	     (chickadee graphics text)
 	     (chickadee graphics path)
 	     (chickadee graphics color)
+	     (chickadee graphics texture)
+	     (chickadee graphics sprite)
+	     (chickadee graphics particles)
 	     (chickadee audio)
 	     (ping collisions)
 	     (ping record-types))
-
-;;; INIT VALUES
-(define w 640)
-
-(define h 480)
 
 ;;; UTILITY FUNCTIONS
 (define (centroid w h)
@@ -26,21 +24,17 @@
 (define (tie-mouse-x rect)
   "Tie a RECT to the mouse on the X axis."
   (set-rect-x! rect (max 0 (min (- (mouse-x) (/ (rect-width rect) 2))
-				(- w (rect-width rect))))))
+				(- (window-width (current-window)) (rect-width rect))))))
 
 ;;; REPL LOOP
 (define repl (spawn-coop-repl-server))
 
 ;;; GAME VARS
-(define ping-ball
-  (make-ball (centroid w h)
-	     10.0 + +))
+(define ping-ball #:not-init)
 
-(define ping-racket
-  (rect 0.0 5.0 100.0 5.0))
+(define ping-racket #:not-init)
 
-(define ping-racket2
-  (rect 0.0 (- h 10.0) 100.0 5.0))
+(define ping-racket2 #:not-init)
 
 (define (make-ball-canvas)
   (make-canvas
@@ -62,27 +56,80 @@
 
 (define game-over #f)
 
-(define print-snippets #f)
+(define game #:FROG)
+
+(define frog-idle-index 0)
+(define (init-frog-pos) (vec2 10 (vec2-y (centroid (window-width (current-window)) (window-height (current-window))))))
+(define frog-position #:not-init)
+(define frog-status #:IDLE)
+
+(define frog-wall (rect 0 0 320 320))
+
+;;; DRAW FUNCTIONS
+(define (draw-frog)
+  ;; draw the frog idle animation
+  (draw-text "FROG" (vec2 10 10) #:color white)
+  (make-)
+  (cond ((eq? frog-status #:IDLE)
+	 (let ((frog-idle (load-image "assets/images/frog_idle.png")))
+	   (draw-sprite
+	    (make-texture-region frog-idle (rect (* 32 frog-idle-index) 0 32 32))
+	    frog-position
+	    #:scale (vec2 4 4))))
+	((eq? frog-status #:WALKING-RIGHT)
+	 (let ((frog-walking (load-image "assets/images/frog_run.png")))
+	   (draw-sprite
+	    (make-texture-region frog-walking (rect (* 32 frog-idle-index) 0 32 32))
+	    frog-position
+	    #:scale (vec2 4 4))))
+	((eq? frog-status #:WALKING-LEFT)
+	 (let ((frog-walking (load-image "assets/images/frog_run.png")))
+	   (draw-sprite
+	    (make-texture-region frog-walking (rect (* 32 frog-idle-index) 0 32 32))
+	    frog-position
+	    #:scale (vec2 4 4))))))
 
 ;;; GAME FUNCTIONS
 (define (draw alpha)
-  (if (not print-snippets)
-      (begin
-	(draw-canvas (make-ball-canvas))
-	(draw-canvas (make-racket-canvas ping-racket))
-	(draw-canvas (make-racket-canvas ping-racket2))
-	(when game-over
-	  (draw-text "     You Lost!\nType R to try again!"
-		     (centroid (- w 200) h)
-		     #:color white)))
-      (snippets w h)))
+  (cond ((eq? game #:PING)
+	 (begin
+	   (draw-canvas (make-ball-canvas))
+	   (draw-canvas (make-racket-canvas ping-racket))
+	   (draw-canvas (make-racket-canvas ping-racket2))
+	   (when game-over
+	     (draw-text "     You Lost!\nType R to try again!"
+			(centroid (- (window-width (current-window)) 200) (window-height (current-window)))
+			#:color white))))
+	((eq? game #:SNIPPETS) (snippets (window-width (current-window)) (window-height (current-window))))
+	((eq? game #:FROG)
+	 (draw-frog))))
 
 (define (updates dt)
+  ;; change frogs idle animation
+  (when (and (eq? game #:FROG))
+    (set! frog-idle-index (if (= frog-idle-index 11)
+			      0
+			      (+ frog-idle-index 1)))
+    (cond ((key-pressed? 'l)
+	   (set! frog-position (init-frog-pos)))
+	  ((key-pressed? 'a)
+	   (set-vec2-x! frog-position (if (< (vec2-x frog-position) 0)
+					  (window-width (current-window))
+					  (- (vec2-x frog-position) (* 2 10))))
+	   (set! frog-status #:WALKING-LEFT))
+	  ((key-pressed? 'e)
+	   (set-vec2-x! frog-position (if (> (vec2-x frog-position) (window-width (current-window)))
+					 0
+					  (+ (vec2-x frog-position) (* 2 10))))
+	   (set! frog-status #:WALKING-RIGHT))
+	  (else (set! frog-status #:IDLE))))
+
+  ;; move the racket
   (define (compute-pos pos direction dt speed)
     (direction pos (* speed dt)))
 
   ;; bounce the ball off the walls and the two rackets
-  (bounce-ball ping-ball w h dt (list ping-racket ping-racket2))
+  (bounce-ball ping-ball (window-width (current-window)) (window-height (current-window)) dt (list ping-racket ping-racket2))
   (when follow (tie-mouse-x ping-racket))
   (when follow (tie-mouse-x ping-racket2))
   
@@ -97,18 +144,27 @@
 
   ;; check if the ball is out of the screen
   (let ((ball-pos (ball-pos ping-ball))
-	(screen-rect (rect 0.0 0.0 w h)))
+	(screen-rect (rect 0.0 0.0 (window-width (current-window)) (window-height (current-window)))))
     (when (not (rect-contains? screen-rect (vec2-x ball-pos) (vec2-y ball-pos)))
       (set! game-over #t))))
 
 (define (key-press key modifiers repeat?)
+  ;; change the game
+  (when (eq? key 'f)
+    (set! game #:FROG))
   (when (eq? key 's)
-    (set! print-snippets (not print-snippets)))
-  (when (eq? key 'r)
+    (set! game #:SNIPPETS))
+  (when (eq? key 'p)
+    (set! game #:PING))
+
+  ;; ping game
+  (when (and  (eq? game #:PING)
+	      (eq? key 'r))
     (set! game-over #f)
-    (set-ball-pos! ping-ball (centroid w h))
+    (set-ball-pos! ping-ball (centroid (window-width (current-window)) (window-height (current-window))))
     (set-ball-direction-x! ping-ball +)
     (set-ball-direction-y! ping-ball +))
+  
   (when (eq? key 'q)
     (abort-game)))
 
@@ -116,16 +172,27 @@
 ;; (define sample (load-audio "assets/audio.mp3"))
 ;; (audio-play sample)
 
-(run-game #:update (lambda (dt) 
-		     (poll-coop-repl-server repl)
-		     (updates dt))
-	  #:key-press (lambda (key modifiers repead?)
-			(key-press key modifiers repead?))
-	  #:mouse-press (lambda (button clicks x y)
-			  (set! follow (not follow)))
-	  #:draw (lambda (alpha)
-		   (draw alpha))
-	  #:clear-color black
-	  #:window-title "PING"
-	  #:window-width w
-	  #:window-height h)
+(define (init)
+  (set! ping-ball
+	(make-ball (centroid (window-width (current-window)) (window-height (current-window)))
+		   10.0 + +))
+  (set! ping-racket
+	(rect 0.0 5.0 100.0 5.0))
+  (set! ping-racket2
+	(rect 0.0 (- (window-height (current-window)) 10.0) 100.0 5.0))
+  (set! frog-position (init-frog-pos)))
+
+(run-game
+ #:load (lambda () (init))
+ #:update (lambda (dt) 
+	    (poll-coop-repl-server repl)
+	    (updates dt))
+ #:key-press (lambda (key modifiers repead?)
+	       (key-press key modifiers repead?))
+ #:mouse-press (lambda (button clicks x y)
+		 (set! follow (not follow)))
+ #:draw (lambda (alpha)
+	  (draw alpha))
+ #:clear-color black
+ #:window-fullscreen? #t
+ #:window-title "PING")
